e23b5439807717c71bba996042d5316d49715bb3
ProjectSoft <projectsoft2009@yandex.ru>
2022-04-11 08:00:15 +0400

    update

--- assets/snippets/GetEvoVideo/snippet.GetEvoVideo.php
+++ assets/snippets/GetEvoVideo/snippet.GetEvoVideo.php
@@ -4,5 +4,8 @@ if(!defined('MODX_BASE_PATH')) die('What are you doing? Get out of here!');
 $url = isset($url) ? $url : '';
 $vd = new Video(null, false);
 $arr = $vd->setLink($url);
-
-return $arr['video'] ? $arr['video'].'<p class="text-center"><a href="' . $url . '" target="_blank" savefrom_lm="0">Смотреть на хостинге</a></p>' : "";
+if(is_array($arr)){
+	return $arr['video'] ? $arr['video'] : "";
+}else{
+	return "";
+}
